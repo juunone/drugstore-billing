0dc7d4eb7d965e0e4e61f5f3c0ed1a6120e87b47
juunone <juunone@gmail.com>
2019-07-07 16:13:25 +0900

    Updates

---
+++ precache-manifest.6a414a3d7e4266198953a9687b6c3de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13f8fd8319e1495962f2",
+    "url": "/colavolab-billing/static/js/runtime~main.bfdf8704.js"
+  },
+  {
+    "revision": "dd767f3c28d18bfa69ab",
+    "url": "/colavolab-billing/static/js/main.3fbec98f.chunk.js"
+  },
+  {
+    "revision": "28768df80e5b64c708f5",
+    "url": "/colavolab-billing/static/js/2.b8632daa.chunk.js"
+  },
+  {
+    "revision": "dd767f3c28d18bfa69ab",
+    "url": "/colavolab-billing/static/css/main.19226115.chunk.css"
+  },
+  {
+    "revision": "28768df80e5b64c708f5",
+    "url": "/colavolab-billing/static/css/2.9a744bc4.chunk.css"
+  },
+  {
+    "revision": "e4067894a2dc24a61f2a97eb18d06558",
+    "url": "/colavolab-billing/index.html"
+  }
+];
